ea22e3ae5b29fb36e315526675001c37102f3283
yuanlin2004 <yuanlin2004@outlook.com>
2026-03-24 12:14:14 -0700

    move docs

--- run.sh
+++ run.sh
@@ -7,28 +7,37 @@
 #--provider openrouter \
 #--max-iterations 20
 
-rm -rf /tmp/leo-appworld-runs/appworld-plugin-sanity2/82e2fac_1
+rm -rf /tmp/leo-appworld-runs/appworld-test-normal-base-openrouter-minmax27
 #PYTHONPATH=src python -m leo.cli.main evaluate \
 leo evaluate \
   --environment appworld \
   --appworld-root /Users/yuan/Documents/GitHub/appworld \
+  --experiment-name appworld-test-normal-base-openrouter-minmax27 \
+  --output-root /tmp/leo-appworld-runs \
   --dataset test_normal \
   --task-limit 5 \
-  --experiment-name appworld-test-normal-base-20b \
-  --output-root /tmp/leo-appworld-runs \
   --max-iterations 30 \
   --agent react \
-  --provider ollama \
-  --model 'gpt-oss:20b' \
-  --log-level concise 2>&1 | tee log
+  --provider openrouter \
+  --model minimax/minimax-m2.7 \
+   2>&1 | tee artifacts/log
 
+
+#  --extra-sys-prompt artifacts/code-dict-man.txt \
+#  --log-level concise 2>&1 | tee log
+#  --dataset test_normal \
+#  --task-limit 5 \
+#  --task-id 82e2fac_1 \
+#  --task-limit 5 \
+#  --log-level concise 2>&1 | tee log
+#  --log-level trace 2>&1 | tee log
 #  --extra-sys-prompt sys.txt \
 #  --task-id 325d6ec_3\
-#  --extra-sys-prompt sys.txt \
 #  --task-id 82e2fac_1 \
+#  --extra-sys-prompt sys.txt \
 #  --provider ollama \
 #  --model 'gpt-oss:20b' \
-#  --provider ollama \
+#  --provider openrouter \
 #  --model minimax/minimax-m2.5 \
 #  --model minimax/minimax-m2.7 \
 #  --model openai/gpt-5.3-codex\
